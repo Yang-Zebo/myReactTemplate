719bd2a3d18d8fd22a89dc8674e5066853822c34
yang zebo <1282258957@qq.com>
2023-03-16 16:08:42 +0800

    yzb

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import 'antd/dist/reset.css'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
 
+console.log('import.meta.env', import.meta.env)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ConfigProvider
     theme={ {
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,1 +1,2 @@
-export const getNavItems = () => {}
+export const getNavItems = () => {
+}
