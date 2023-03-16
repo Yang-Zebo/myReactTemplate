268e0969e044f709aa0bff3f6938965e51a29691
yang zebo <1282258957@qq.com>
2023-03-16 18:19:20 +0800

    yzb

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,9 +1,59 @@
 import React from 'react'
+import styles from './index.module.less'
+import { Form, Input, Checkbox, Button } from 'antd'
+import { LockOutlined, UserOutlined } from '@ant-design/icons'
 
 const Index = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+  }
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo)
+  }
   return (
-    <div>
-      login
+    <div className={ styles.loginContainer }>
+      <div className={ styles.formBox }>
+        <div className={ styles.title }>登录/注册</div>
+        <Form
+          name="basic"
+          size="large"
+          initialValues={ { remember: true } }
+          onFinish={ onFinish }
+          onFinishFailed={ onFinishFailed }
+          labelCol={ { span: 6 } }
+          autoComplete="off"
+        >
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={ [{ required: true, message: 'Please input your username!' }] }
+          >
+            <Input prefix={ <UserOutlined/> }/>
+          </Form.Item>
+
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={ [{ required: true, message: 'Please input your password!' }] }
+          >
+            <Input.Password prefix={ <LockOutlined/> }/>
+          </Form.Item>
+
+          <Form.Item wrapperCol={ { offset: 6 } } name="remember">
+            <Checkbox>记住账号</Checkbox>
+          </Form.Item>
+
+          <Form.Item wrapperCol={ { offset: 6 } }>
+            <Button type="primary" htmlType="submit" style={ { marginRight: '10px' } }>
+              登录
+            </Button>
+            <Button htmlType="submit">
+              注册
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   )
 }
